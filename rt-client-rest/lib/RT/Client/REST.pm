42fceb8a04fd8c33b06f62110438a2ac02f65b34
pplusdomain <pplusdomain@gmail.com>
2012-10-27 07:12:07 +0000

    Apply patch from MRUIZ for treating non-latin1 strings

--- rt-client-rest/lib/RT/Client/REST.pm
+++ rt-client-rest/lib/RT/Client/REST.pm
@@ -517,6 +517,9 @@ sub _submit {
 
     # Now, we construct the request.
     if (@$data) {
+        # The request object expects "bytes", not strings
+        map { utf8::encode($_) } @$data;
+
         $req = POST($self->_uri($uri), $data, Content_Type => 'form-data');
     }
     else {
