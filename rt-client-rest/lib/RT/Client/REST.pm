54c036e39af78004f3f1f3d2044c5df1fa9727b9
Dmitri Tikhonov <dmitri@cpan.org>
2014-05-10 07:49:25 -0400

    Version 0.49

--- rt-client-rest/lib/RT/Client/REST.pm
+++ rt-client-rest/lib/RT/Client/REST.pm
@@ -24,7 +24,7 @@ use strict;
 use warnings;
 
 use vars qw/$VERSION/;
-$VERSION = '0.48';
+$VERSION = '0.49';
 $VERSION = eval $VERSION;
 
 use Error qw(:try);
@@ -1234,7 +1234,7 @@ RT server, which is either good or bad, depending how you look at it.
 
 =head1 VERSION
 
-This is version 0.48 of B<RT::Client::REST>.
+This is version 0.49 of B<RT::Client::REST>.
 
 =head1 AUTHORS
 
